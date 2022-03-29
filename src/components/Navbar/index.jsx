222dac7e1c80cf4326317a0cd38c75e3b41fce70
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-29 11:36:32 +0530

    feat: basic navbar component and styles

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar flex-row flex-align-center flex-justify-between">
+        <h5 className="logo">inscribe</h5>
+    </nav>
+  )
+}
+
+export { Navbar };
