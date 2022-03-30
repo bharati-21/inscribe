84671f432432d11486d0b431c369d2db1adb1e7f
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-30 10:15:25 +0530

    feat: component for pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <section className="section-wrapper flex-col flex-align-center flex-justify-center">
+            Home
+        </section>
+    )
+}
+
+export { Home };
