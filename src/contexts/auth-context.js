9aa24a8bf1e486e990d524a6eedd3b64bdd002bf
Bharati Subramanian <bharatisharada@gmail.com>
2022-03-30 10:12:32 +0530

    feat: auth context

---
+++ src/contexts/auth-context.js
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { initialAuthState, authReducerFunction } from "reducers/";
+
+const AuthContext = createContext(initialAuthState);
+const { Provider } = AuthContext;
+
+
+const AuthProvider = ({ children }) => {
+
+    const getInitialAuthState = () => {
+		const authToken = localStorage.getItem("inscribe-token");
+		const authUser = localStorage.getItem("inscribe-user");
+
+        if (authToken) {
+            return {
+                authUser,
+                authToken,
+                authError: false,
+                authLoading: false,
+                isAuth: true
+            }
+		}
+        return initialAuthState;
+    }
+
+    const [authState, authDispatch] = useReducer(
+		authReducerFunction,
+		getInitialAuthState()
+	);
+
+	return <Provider value={{ authState, authDispatch }}>{children}</Provider>;
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
