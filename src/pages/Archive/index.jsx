238ef20ba10343a404ffe42fba32e62a35e96b2f
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-02 10:39:00 +0530

    feat: display archived note list

--- src/pages/Archive/index.jsx
+++ src/pages/Archive/index.jsx
@@ -1,9 +1,21 @@
+import { NotesList } from "components";
+import { useNotes } from "contexts/";
+
 const Archive = () => {
-    return (
-        <section className="section-wrapper flex-col flex-align-center flex-justify-center">
-            Archive
-        </section>
-    )
-}
+    const { archives } = useNotes();
+	return (
+		<section className="section-wrapper flex-col flex-align-center flex-justify-start">
+			<div className="notes-list-wrapper">
+				{archives.length ? (
+					<NotesList notes={archives} />
+				) : (
+					<p className="text-lg text-center">
+						You don't have any archived notes!
+					</p>
+				)}
+			</div>
+		</section>
+	);
+};
 
 export { Archive };
