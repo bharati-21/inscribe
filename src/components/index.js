155bc721ce5883919f880a721c1902a4e07a94f6
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-03 11:58:32 +0530

    feat: export LabelOptions to common component file

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { NoteItem } from './Notes/NoteItem/';
 export { NotesList } from './Notes/NoteList/';
 export { NewNoteModal } from './Notes/NewNoteModal/';
 export { ColorPalette } from './ColorPalette';
+export { LabelOptions } from './LabelOptions';
