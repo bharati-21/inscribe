527d8d801a0218a7b7ab47cca36bc4ddb5fb3a1b
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-04 23:40:51 +0530

    refactor: add error/ loading message

--- src/pages/Archive/index.jsx
+++ src/pages/Archive/index.jsx
@@ -2,18 +2,39 @@ import { NotesList } from "components";
 import { useNotes } from "contexts/";
 
 const Archive = () => {
-    const { archives } = useNotes();
+    const { archives, notesStateLoading, notesStateError } = useNotes();
+
+	const loadingMessage = (
+		<div className="message">
+			<p className="success-color text-lg my-1">Loading Archived Notes...</p>
+		</div>
+	);
+
+	const errorMessage = (
+		<div className="message">
+			<p className="error-color text-lg my-1">{notesStateError}</p>
+		</div>
+	);
+
 	return (
 		<section className="section-wrapper flex-col flex-align-center flex-justify-start">
-			<div className="notes-list-wrapper">
-				{archives.length ? (
-					<NotesList notes={archives} />
-				) : (
-					<p className="text-lg text-center">
-						You don't have any archived notes!
-					</p>
-				)}
-			</div>
+            {
+                notesStateLoading ? (
+                    loadingMessage
+                ) : notesStateError ? (
+                    errorMessage
+                ) : (
+                    <div className="notes-list-wrapper">
+                        {archives.length ? (
+                            <NotesList notes={archives} />
+                        ) : (
+                            <p className="text-lg text-center">
+                                You don't have any archived notes!
+                            </p>
+                        )}
+                    </div>
+                )
+            }
 		</section>
 	);
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,35 +2,38 @@ import { NotesList } from "components";
 import { useNotes } from "contexts/";
 
 const Home = () => {
-	const { notes, notesLoading, notesError } = useNotes();
+	const { notes, notesStateLoading, notesStateError } = useNotes();
 
-	const loadingMessage = notesLoading && (
+	const loadingMessage = (
 		<div className="message">
 			<p className="success-color text-lg my-1">Loading Notes...</p>
 		</div>
 	);
-	const errorMessage = notesError && (
+	const errorMessage = (
 		<div className="message">
-			<p className="error-color text-lg my-1">{notesError}</p>
+			<p className="error-color text-lg my-1">{notesStateError}</p>
 		</div>
 	);
 
 	return (
 		<section className="section-wrapper flex-col flex-align-center flex-justify-start">
-			{loadingMessage ? (
-				loadingMessage
-			) : (
-				<>
-					{errorMessage}
-					<div className="notes-list-wrapper">
-						{notes.length ? (
-							<NotesList notes={notes} />
-						) : (
-							<p className="text-lg text-center">You don't have any notes!</p>
-						)}
-					</div>
-				</>
-			)}
+			{
+                notesStateLoading ? (
+                    loadingMessage
+                ) : notesStateError ? (
+                    errorMessage
+                ) : (
+                    <div className="notes-list-wrapper">
+                        {notes.length ? (
+                            <NotesList notes={notes} />
+                        ) : (
+                            <p className="text-lg text-center">
+                                You don't have any notes!
+                            </p>
+                        )}
+                    </div>
+			    )
+            }
 		</section>
 	);
 };
