fd5b238d286bd6f0fce9dad7d651f25eaa1354aa
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-05 13:42:07 +0530

    feat: controllers to get, delete, post to trash

---
+++ src/backend/controllers/TrashController.js
@@ -0,0 +1,95 @@
+import { Response } from "miragejs";
+import { requiresAuth } from "../utils/authUtils";
+
+/**
+ * All the routes related to Trash are present here.
+ *  These are Privately accessible routes.
+ * */
+
+/**
+ * This handler handles gets all trashed notes in the db.
+ * send GET Request at /api/trash
+ * */
+
+ export const getAllTrashedNotesHandler = function (schema, request) {
+	const user = requiresAuth.call(this, request);
+	if (!user) {
+		return new Response(
+			404,
+			{},
+			{
+				errors: [
+					"The email you entered is not Registered. Not Found error",
+				],
+			}
+		);
+	}
+	return new Response(200, {}, { trash: user.trash });
+};
+
+
+/**
+ * This handler handles restores an archived note from the trash and returns the updated archives.
+ * send POST Request at /api/trash/restore/:noteId
+ * */
+
+export const restoreFromTrashHandler = function (schema, request) {
+	const user = requiresAuth.call(this, request);
+	try {
+		if (!user) {
+			return new Response(
+				404,
+				{},
+				{
+					errors: [
+						"The email you entered is not Registered. Not Found error",
+					],
+				}
+			);
+		}
+		const { noteId } = request.params;
+		const noteTobeRestored = user.trash.find(note => note._id === noteId);
+        if(noteTobeRestored.isArchived()) {
+            user.archives.push({ ...noteTobeRestored });
+        }
+        else {
+            user.notes.push({ ...noteTobeRestored });
+        }
+        user.trash = user.trash.filter(note => note._id !== note);
+	    this.db.users.update({ _id: user._id }, user);
+        if(noteTobeRestored.isArchived) return new Response(201, {}, { archives: user.archives, trash: user.trash })
+		return new Response(201, {}, { notes: user.notes, trash: user.trash });
+	} catch (error) {
+		return new Response(
+			500,
+			{},
+			{
+				error,
+			}
+		);
+	}
+};
+
+/**
+ * This handler handles deletes note from trash forever.
+ * send DELETE Request at /api/trash/delete/:noteId
+ * */
+
+export const deleteFromTrashHandler = function (schema, request) {
+	const user = requiresAuth.call(this, request);
+	if (!user) {
+		return new Response(
+			404,
+			{},
+			{
+				errors: [
+					"The email you entered is not Registered. Not Found error",
+				],
+			}
+		);
+	}
+	const { noteId } = request.params;
+    user.trash = user.trash.filter(note => note._id !== noteId);
+	this.db.users.update({ _id: user._id }, user);
+	return new Response(200, {}, { trash: user.trash });
+};
