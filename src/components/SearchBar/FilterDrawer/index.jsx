d1215e42ba56fda5eecb8d63e93b09da7e68249a
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-06 13:48:57 +0530

    feat: sort, filter by priorities

--- src/components/SearchBar/FilterDrawer/index.jsx
+++ src/components/SearchBar/FilterDrawer/index.jsx
@@ -4,7 +4,9 @@ import { useNotes } from "contexts";
 
 const FilterDrawer = ({ handleChangeShowFilterModal }) => {
 
-    const { labels, filterByLabel, notesDispatch, sortBy } = useNotes();
+    const { labels, filterByLabel, notesDispatch, sortBy, filterByPriority } = useNotes();
+
+    const {  sortByDate, sortByPriority } = sortBy;
 
     const handleFilterByLabelChange = labelId => {
         const newFilterByLabels = filterByLabel.map(filter => filter.id === labelId ? { ...filter, filtered: !filter.filtered } : filter);
@@ -17,8 +19,19 @@ const FilterDrawer = ({ handleChangeShowFilterModal }) => {
         });
     }
 
-    const handleSortByChange = ({ target: { value } }) => {
-        notesDispatch({ action: { type: 'SORT_BY', payload: { sortBy: value } } });
+    const handleFilterByPriorityChange = priorityId => {
+        const newFilterByPriority = filterByPriority.map(priority => priority.id === priorityId ? { ...priority, filtered: !priority.filtered } : priority);
+
+        notesDispatch({
+            action: { 
+                type: "FILTER_BY_PRIORITY", 
+                payload: { filterByPriority: newFilterByPriority }
+            }
+        })
+    }
+
+    const handleSortByChange = ({ target: { value, name } }) => {
+        notesDispatch({ action: { type: 'SORT_BY', payload: { sortBy: { ...sortBy, [name]: value } } } });        
     }
 
 	return (
@@ -26,53 +39,117 @@ const FilterDrawer = ({ handleChangeShowFilterModal }) => {
 			<div className="filter-sort-drawer py-0-25">
 				<div className="filter-sort-drawer-head py-0-75 px-0-75 flex-row flex-align-center flex-justify-between">
 					<h6>Filter &amp; Sort Notes</h6>
-					<button className="btn-icon btn btn-light" onClick={e => handleChangeShowFilterModal('CLOSE_FILTER_MODAL')}>
+					<button
+						className="btn-icon btn btn-light"
+						onClick={(e) =>
+							handleChangeShowFilterModal("CLOSE_FILTER_MODAL")
+						}
+					>
 						<Close color="white" />
 					</button>
 				</div>
 				<div className="filter-sort-options py-0-75 px-0-75 flex-col flex-align-start flex-justify-center">
 					<div className="sort-options">
-						<p className="sort-head text-reg">Sort By</p>
+						<p className="sort-head text-reg">Sort By Date</p>
 						<select
-							name="sort-by"
+							name="sortByDate"
 							id="sort-by"
 							className="dropdown sorting-dropdown px-0-5 py-0-25 mt-0-25 text-sm"
-                            onChange={handleSortByChange}
-                            value={sortBy}
+							onChange={handleSortByChange}
+							value={sortByDate}
 						>
-                            <option value="" name="sort-by" defaultValue disabled>
-                                --Chose an option--
-                            </option>
-							<option value="Newest First" name="sort-by">
+							<option
+								value=""
+								name="sortByDate"
+								defaultValue
+								disabled
+							>
+								--Chose an option--
+							</option>
+							<option value="Newest First" name="sortByDate">
 								Newest First
 							</option>
-							<option value="Oldest First" name="sort-by">
+							<option value="Oldest First" name="sortByDate">
 								Oldest First
 							</option>
 						</select>
 					</div>
-					{
-                        labels.length > 0 && (
-                            <div className="filter-options">
-                                <p className="filter-head text-reg">
-                                    Filter By Labels
-                                </p>
-                                <div className="label-checkbox-wrapper flex-row flex-align-center flex-justify-start mt-0-25 flex-wrap">
-                                    {labels.map(({ label, id }) => (
-                                        <label className="text-sm flex-row flex-align-center flex-justify-start" key={id}>
-                                            <input
-                                                type="checkbox"
-                                                value={label}
-                                                checked={filterByLabel.find(filter => filter.id === id).filtered}
-                                                onChange={() => handleFilterByLabelChange(id)}
-                                            />
-                                            {label}
-                                        </label>
-                                    ))}
-                                </div>
-                            </div>
-                        )
-					}
+                    <div className="sort-options">
+						<p className="sort-head text-reg">Sort By Priority</p>
+						<select
+							name="sortByPriority"
+							id="sort-by"
+							className="dropdown sorting-dropdown px-0-5 py-0-25 mt-0-25 text-sm"
+							onChange={handleSortByChange}
+							value={sortByPriority}
+						>
+							<option
+								value=""
+								name="sortByPriority"
+								defaultValue
+								disabled
+							>
+								--Chose an option--
+							</option>
+							<option value="High to Low" name="sortByPriority">
+                                High to Low
+							</option>
+							<option value="Low to High" name="sortByPriority">
+                                Low to High
+							</option>
+						</select>
+					</div>
+					{labels.length > 0 && (
+						<div className="filter-options">
+							<p className="filter-head text-reg">
+								Filter By Labels
+							</p>
+							<div className="label-checkbox-wrapper flex-row flex-align-center flex-justify-start mt-0-25 flex-wrap">
+								{labels.map(({ label, id }) => (
+									<label
+										className="text-sm flex-row flex-align-center flex-justify-start"
+										key={id}
+									>
+										<input
+											type="checkbox"
+											value={label}
+											checked={
+												filterByLabel.find(
+													(filter) => filter.id === id
+												).filtered
+											}
+											onChange={() =>
+												handleFilterByLabelChange(id)
+											}
+										/>
+										{label}
+									</label>
+								))}
+							</div>
+						</div>
+					)}
+
+					<div className="filter-options">
+						<p className="filter-head text-reg">Filter By Priority</p>
+						<div className="label-checkbox-wrapper flex-row flex-align-center flex-justify-start mt-0-25 flex-wrap">
+							{filterByPriority.map(({ id, priority, filtered }) => (
+								<label
+									className="text-sm flex-row flex-align-center flex-justify-start"
+									key={id}
+								>
+									<input
+										type="checkbox"
+										value={priority}
+										checked={filtered}
+										onChange={() =>
+											handleFilterByPriorityChange(id)
+										}
+									/>
+									{priority}
+								</label>
+							))}
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -6,8 +6,10 @@ import { FilterDrawerPortal } from "./FilterDrawer/FilterDrawerPortal";
 import "./search-bar.css";
 
 const SearchBar = ({ noteType }) => {
-	const { searchText, handleChangeSearchText, archives, notes, sortBy, filterByLabel, notesDispatch } =
+	const { searchText, handleChangeSearchText, archives, notes, sortBy, filterByLabel, notesDispatch, filterByPriority } =
 		useNotes();
+
+    const { sortByDate, sortByPriority } = sortBy;
 
 	const [showFilterModal, setShowFilterModal] = useState(false);
 
@@ -29,8 +31,12 @@ const SearchBar = ({ noteType }) => {
 			action: {
 				type: "RESET_FILTERS",
 				payload: {
-					sortBy: "",
+                    sortBy: {sortByDate: '', sortByPriority: ''}, 
 					filterByLabel: filterByLabel.map((filter) => ({
+						...filter,
+						filtered: false,
+					})),
+                    filterByPriority: filterByPriority.map((filter) => ({
 						...filter,
 						filtered: false,
 					})),
@@ -68,20 +74,32 @@ const SearchBar = ({ noteType }) => {
 			)}
 			<div className="text-clear-button-wrapper flex-row flex-justify-between flex-align-start mx-auto mt-1">
 				<div className="sort-filter-text">
-					{sortBy && (
-						<p className="sort-options-text">Sort by- {sortBy}</p>
+                    {sortByDate && (
+                            <p className="sort-options-text">Sort by- {sortByDate}</p>
+                    )}
+					{sortByPriority && (
+						<p className="sort-options-text">Sort by- {sortByPriority}</p>
 					)}
 					{filterByLabel.some((filter) => filter.filtered) && (
 						<div className="filter-options-text flex-row flex-row flex-align-center flex-justify-start">
-							Filter by -{" "}
+							Filter by Label- 
 							{filterByLabel.map(
 								({ id, label, filtered }) =>
 									filtered && <span key={id}>{label}</span>
 							)}
 						</div>
 					)}
+                    {filterByPriority.some((filter) => filter.filtered) && (
+						<div className="filter-options-text flex-row flex-row flex-align-center flex-justify-start">
+							Filter by Priority- 
+							{filterByPriority.map(
+								({ id, priority, filtered }) =>
+									filtered && <span key={id}>{priority}</span>
+							)}
+						</div>
+					)}
 				</div>
-				{sortBy || filterByLabel.some((filter) => filter.filtered) ? (
+				{sortByPriority || sortByDate || filterByLabel.some((filter) => filter.filtered) || filterByPriority.some(filter => filter.filtered) ? (
 					<button
 						className="btn btn-secondary text-sm px-0-5 py-0-25"
 						onClick={handleClearFilters}
