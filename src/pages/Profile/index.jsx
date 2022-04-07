cb00aa6be13f7f61676ed2d2bbdc13fe063a71a7
Bharati Subramanian <bharatisharada@gmail.com>
2022-04-07 20:09:55 +0530

    feat: added simple profile message

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,11 @@
+import { useAuth } from "contexts";
+
 const Profile = () => {
+    const { authUser } = useAuth();
+
     return (
         <section className="section-wrapper flex-col flex-align-center flex-justify-center">
-            Profile
+           <h3 className="text-center">👋 Welcome to inscribe profile! {authUser.firstName} {authUser.lastName}</h3>
         </section>
     )
 }
