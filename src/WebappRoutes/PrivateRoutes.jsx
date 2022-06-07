8ef77e40bba9f8a7d18841011f5f0b25bce70150
Bharati Subramanian <bharatisharada@gmail.com>
2022-06-07 10:20:15 +0530

    fix: set state, replace props in Navigate

--- src/WebappRoutes/PrivateRoutes.jsx
+++ src/WebappRoutes/PrivateRoutes.jsx
@@ -1,9 +1,10 @@
 import { useAuth } from "contexts";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { Sidebar } from "components/";
 
 const PrivateRoutes = () => {
 	const { isAuth } = useAuth();
+	const location = useLocation();
 
 	return isAuth ? (
 		<main className="grid-container">
@@ -11,7 +12,7 @@ const PrivateRoutes = () => {
 			<Outlet />
 		</main>
 	) : (
-		<Navigate to="/login" />
+		<Navigate to="/login" state={{ from: location }} replace />
 	);
 };
 
